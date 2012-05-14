089f9a9126244c5ffeebf3e922a67fa3a75d66f7
Yoann Laissus <yoann.laissus@gmail.com>
2012-05-14 19:26:41 +0200

    Add again the GPL license removed by mistake

--- manifests/init.pp
+++ manifests/init.pp
@@ -1,4 +1,14 @@
-# Class: resolvconf
+# resolvconf module
+#
+# Copyright 2008, admin(at)immerda.ch
+# Copyright 2008, Puzzle ITC GmbH
+# Marcel Härry haerry+puppet(at)puzzle.ch
+# Simon Josi josi+puppet(at)puzzle.ch
+#
+# This program is free software; you can redistribute 
+# it and/or modify it under the terms of the GNU 
+# General Public License version 3 as published by 
+# the Free Software Foundation.
 #
 # This class handles configuring /etc/resolv.conf
 #
